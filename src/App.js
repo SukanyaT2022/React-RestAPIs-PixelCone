71ade8255123301f1041b91e9e6b7120cdb8e9c8
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-10-30 12:51:19 -0400

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import './App.css'
 
 const App = () => {
-
     const apiKey ="c19t5LBUeXItCyO5nb5bpds50rtTKwmX7xOeFa4PnEBN8jabAxpjQW6U"
-    const searchQuery = 'dog';
-    const countPicPerPage = 5;
+    const searchQuery = 'cat';
+    const countPicPerPage = 12;
     const apiUrl = `https://api.pexels.com/v1/search?query=${searchQuery}&per_page=${countPicPerPage}`;
     const[data,setData]=useState()
 
@@ -25,13 +25,12 @@ const App = () => {
     },[])
 
   return (
-    <div>
+      <div className='wrapper'>
         {data &&
             data.map((val)=>(
                 // <div>{val}</div>
-              <img src={val.src.tiny}/>
+              <img src={val.src.tiny} className='image'/>
             ))
-
         }
     </div>
   )
