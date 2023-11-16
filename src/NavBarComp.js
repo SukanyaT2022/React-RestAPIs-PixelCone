633f0e44a0d679517cfed25660268645e96516cf
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-11-16 06:39:53 -0500

    add

--- src/NavBarComp.js
+++ src/NavBarComp.js
@@ -9,7 +9,7 @@ function NavBarComp() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home"><img src={logo} className='targetLogo'/><b>Photo Search</b></Navbar.Brand>
+        <Navbar.Brand href="#home"><img src={logo} className='targetLogo'/><b className='targetB'>Photo Search</b></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
@@ -19,15 +19,12 @@ function NavBarComp() {
           <Nav>
             <div className='targetT'>T</div>
           <NavDropdown title="" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Sign In</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+               Sign Out
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+            
+             
             </NavDropdown>
             <Nav.Link eventKey={2} href="#memes">
          <button className='uploadButton'>Upload</button>
